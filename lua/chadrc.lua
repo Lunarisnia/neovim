e6ad062b51a423ba3393a580e479830ec63881ff
Louna <louna@Lounas-MacBook-Air.local>
2024-04-13 15:37:51 +0700

    changed the header and dashboard is now default on startup

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,16 +1,37 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "pastelDark",
+  theme = "pastelDark",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+
+  nvdash = {
+    load_on_startup = true,
+    header = {
+      " ._       __          ____                               ",
+      ";  `\\--,-' /`)    _.-'    `-._                           ",
+      " \\_/    ' | /`--,'            `-.     .--....____        ",
+      "  /                              `._.'           `---... ",
+      "  |-.   _      ;                        .-----..._______)",
+      ",,\\q/ (q_>'_...                      .-'                 ",
+      "===/ ; _.-'~~-             /       ,'                    ",
+      '`""`-\'_,;  `""         ___(       |                      ',
+      "         \\         ; /'/   \\      \\                      ",
+      "          `.      //' (    ;`\\    `\\                     ",
+      "          / \\    ;     `-  /  `-.  /                     ",
+      "         (  (;   ;     (__/    /  /                      ",
+      "          \\,_)\\  ;           ,'  /                       ",
+      "  .-.MY         |  |           `--'                       ",
+      " (BS BUG)-.    (__,>                                      ",
+    },
+  },
 }
 
 return M
